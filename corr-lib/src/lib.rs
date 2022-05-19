7c7f4871a26a550507efe8a0493f1fd3a22fe0bc
ATM <naik.atmaram@gmail.com>
2022-05-19 14:22:06 +0530

    ATM | while step

--- corr-lib/src/lib.rs
+++ corr-lib/src/lib.rs
@@ -17,6 +17,7 @@ pub fn get_keywords<'a>()->Vec<&'a str>{
 }
 pub fn get_journey_keywords<'a>()->Vec<&'a str>{
     return vec![
+        "while",
         "undef",
         "measure",
         "ingest",
