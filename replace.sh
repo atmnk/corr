5bb4f7159d8e606497de2b18b2d44f274d1d3834
ATM <naik.atmaram@gmail.com>
2022-07-23 22:20:29 +0530

    ATM | fire and forget ingest

--- replace.sh
+++ replace.sh
@@ -1,4 +1,4 @@
 cargo build --release
-rm -rf /opt/homebrew/Cellar/corr/1.0.3/bin/corr
+rm -rf /opt/homebrew/Cellar/corr/1.1.1/bin/corr
 ./target/release/corr --help
-sudo cp ./target/release/corr /opt/homebrew/Cellar/corr/1.0.3/bin
+sudo cp ./target/release/corr /opt/homebrew/Cellar/corr/1.1.1/bin
